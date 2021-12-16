0d0c9f8567b2cb1602babfd66373b9285994447f
Rahul Krishna <rahulkrishna.web@gmail.com>
2021-12-16 07:12:29 +0530

    Get to deploy

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useRouter } from 'next/router';
 import fb from '../firebase/clientApp';
 import { getAuth, signOut } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -9,11 +8,7 @@ const Profile = () => {
     const router = useRouter();
     const auth = getAuth(fb);
     const [user] = useAuthState(auth);
-    if (!user) {
-        router.push({
-            pathname: '/auth'
-        })
-      }
+    
     return(
         <div>
             {user && <ProfileScreen />}
